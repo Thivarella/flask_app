69176879f0cd4434390db34c489116f541f54ab2
thiago.varella <thiago.varella@monitoratec.com.br>
2019-08-09 20:08:08 -0300

    db

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_DATABASE_URI = 'postgres://' \
-                          'uowkyanafvnwzr:25df53596c0335ef6e817d23aeffcf95275880a11e08848a7bd70ce42638d25e' \
-                          '@ec2-54-227-251-33.compute-1.amazonaws.com:5432/deubhu9odd5sbb'
+                          'mzuhlaakcobfqr:9453564fe67a793e8e8a44767ee273a32ef9d599d2aaf2ffb40f033659488205@' \
+                          'ec2-107-21-126-201.compute-1.amazonaws.com:5432/ddv0pqpslqm7bi'
