f0e5e7f30b683f96f79b222da78deef9e54d1a8c
thiago.varella <thiago.varella@monitoratec.com.br>
2019-08-11 22:07:22 -0300

    a

--- app.py
+++ app.py
@@ -1,28 +1,6 @@
 from flask import Blueprint
 from flask_restful import Api
 from resources import ClienteResource, ProdutoResource, PedidoResource
-import os
-from flask import Flask
-
-
-def create_app(config_filename):
-    app = Flask(__name__)
-    app.config.from_object(config_filename)
-
-    from app import api_bp
-    app.register_blueprint(api_bp)
-
-    from models.Model import db
-    db.init_app(app)
-
-    return app
-
-
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app = create_app("config")
-    app.run(port=port)
-
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
